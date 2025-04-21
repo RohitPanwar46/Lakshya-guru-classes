4a9454a4fdbf23b1438c8b872b791ec453dc4e7e
Rohit <freelancerrohit46@gmail.com>
2025-04-21 17:07:15 +0530

    some more changes

--- components/Card.js
+++ components/Card.js
@@ -2,10 +2,22 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className='Card w-24 mx-auto bg-[#c1fd50] rounded-xl my-5 '>
+    <div className='cards flex '>
+    <div className='Card w-84 h-42 mx-auto bg-[#c1fd50] rounded-xl my-7 px-5 py-3 flex justify-center items-center gap-5 flex-col '>
         <h1>Get Our Upcoming course details</h1>
         <div className="details">4Th grade teacher vaccancy prepairetion</div>
         <div className="date">15 july 2025</div>
+    </div>
+    <div className='Card w-84 h-42 mx-auto bg-[#c1fd50] rounded-xl my-7 px-5 py-3 flex justify-center items-center gap-5 flex-col '>
+        <h1>Get Our Upcoming course details</h1>
+        <div className="details">4Th grade teacher vaccancy prepairetion</div>
+        <div className="date">15 july 2025</div>
+    </div>
+    <div className='Card w-84 h-42 mx-auto bg-[#c1fd50] rounded-xl my-7 px-5 py-3 flex justify-center items-center gap-5 flex-col '>
+        <h1>Get Our Upcoming course details</h1>
+        <div className="details">4Th grade teacher vaccancy prepairetion</div>
+        <div className="date">15 july 2025</div>
+    </div>
     </div>
   )
 }
